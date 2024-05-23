288988fe834d2e04e08ed5c20969de6d44908414
Tuananh <vutuananhqn1368@gmail.com>
2024-05-23 13:12:48 +0700

    edit ir sensor, relay and servo

--- Hardwares/ir_sensor.h
+++ Hardwares/ir_sensor.h
@@ -1,10 +1,7 @@
-#ifndef IR_SENSOR_H
 #define IR_SENSOR_H
 
 #include "stm32f10x.h"
-#include "gpio.h"
+
 void ir_sensors_init(void);
 uint8_t ir1_sensor_read(void);
 uint8_t ir2_sensor_read(void);
-
-#endif
